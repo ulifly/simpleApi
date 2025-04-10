1b16490ec5f6528a234a67328f2c778a8810d99e
Ulifly <ulifly@hotmail.com>
2025-04-10 08:06:54 -0600

    testing routes

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 import http from 'node:http';
 import aviablePort from './src/aviablePort.js';
+import fs from 'node:fs';
 
 const lookingPort = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
-    console.log('request received', req.url);
-    res.end('Hello World!');
-});
+const processRequest = (req, res) => {
+    if(req.url === '/'){
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end('<h1> Bienvenido a mi página de inicio</h1> <a href="/contacto">Contacto</a>');
+    
+    } else if (req.url === '/contacto'){
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end('<h1>contacto</h1> <a href="/">Inicio</a>');
+    }
+    else if(req.url === '/img'){
+        fs.readFile('./src/tts.png', (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/html; charset=utf-8');
+                res.end('<h1> 500 something happend</h1>');
+                return;
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'image/png');
+                res.end(data);
+            }
+        })
+        res.setHeader('Content-Type', 'image/png');
+        
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end('<h1> 404 </h1>');
+    }
+}
 
-
+const server = http.createServer(processRequest);
 
 aviablePort(lookingPort).then(port => {
     server.listen(port, () => {
